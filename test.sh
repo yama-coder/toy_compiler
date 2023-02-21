ca264aa1f16c90606b6e6b688934dda956d97b4c
yama-coder <yamako6419@gmail.com>
2023-02-22 01:09:31 +0900

    add unary operator

--- test.sh
+++ test.sh
@@ -22,5 +22,7 @@ assert 21 "5+20-4"
 assert 41 " 12 + 34 - 5 "
 assert 47 "5 + 6 * 7"
 assert 4 '(3+5)/2'
+assert 10 '-10 + 20'
+assert 30 '+5+25'
 
 echo OK
--- tmp.s
+++ tmp.s
@@ -1,17 +1,11 @@
 .intel_syntax noprefix
 .globl main
 main:
-  push 3
   push 5
+  push 25
   pop rdi
   pop rax
   add rax, rdi
-  push rax
-  push 2
-  pop rdi
-  pop rax
-  cqo
-  idiv rdi
   push rax
   pop rax
   ret
